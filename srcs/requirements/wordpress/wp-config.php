3a3d434e986db1549eeb072e8937747f57cf1e8c
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2025-03-22 20:08:42 +0000

    redis done

--- srcs/requirements/wordpress/wp-config.php
+++ srcs/requirements/wordpress/wp-config.php
@@ -1,28 +1,36 @@
 <?php
-define( 'DB_NAME', 'CHNAME');
-define( 'DB_USER', 'CHUSER');
-define( 'DB_PASSWORD', 'CHPASS');
-define( 'DB_HOST', 'CHHOST');
+
+define( 'DB_NAME', 'wordpress_db' );
+define( 'DB_USER', 'youmoukh' );
+define( 'DB_PASSWORD', '656656' );
+define( 'DB_HOST', 'mariadb' );
 define( 'DB_CHARSET', 'utf8' );
 define( 'DB_COLLATE', '' );
+
 define('WP_HOME','https://youmoukh.42.fr');
 define('WP_SITEURL','https://youmoukh.42.fr');
-define( 'AUTH_KEY',         'put your unique phrase here' );
-define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
-define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
-define( 'NONCE_KEY',        'put your unique phrase here' );
-define( 'AUTH_SALT',        'put your unique phrase here' );
-define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
-define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
-define( 'NONCE_SALT',       'put your unique phrase here' );
+define( 'AUTH_KEY',         '' );
+define( 'SECURE_AUTH_KEY',  '' );
+define( 'LOGGED_IN_KEY',    '' );
+define( 'NONCE_KEY',        '' );
+define( 'AUTH_SALT',        '' );
+define( 'SECURE_AUTH_SALT', '' );
+define( 'LOGGED_IN_SALT',   '' );
+define( 'NONCE_SALT',       '' );
 
-sed -i "define( 'WP_REDIS_HOST', 'redis');
+define( 'WP_REDIS_HOST', 'redis');
 define( 'WP_REDIS_PORT', 6379);
+define('WP_REDIS_TIMEOUT', 1); 
 define( 'WP_REDIS_DISABLED', false);    
-define( 'WP_CACHE', true);  
+define('WP_REDIS_PASSWORD', '');  
+define('WP_REDIS_DATABASE', 0);   
 
 $table_prefix = 'wp_';
-define( 'WP_DEBUG', false );
+
+if ( ! defined( 'WP_DEBUG' ) ) {
+	define( 'WP_DEBUG', false );
+}
+
 if ( ! defined( 'ABSPATH' ) ) {
 	define( 'ABSPATH', __DIR__ . '/' );
 }
