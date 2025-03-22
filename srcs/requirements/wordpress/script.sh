352dac8dbe8d0bed30bfc95c7f1e3f4b731222f8
younesmoukhlij <younesmoukhlijofficial@gmail.com>
2025-03-22 02:47:40 +0000

    adding redis

--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-echo "im sleeping"
 sleep 30
-echo " sleeping done "
-
 
 
 wp core download --allow-root
@@ -11,7 +8,7 @@ wp core download --allow-root
 wp config create --dbname=$CONFIGURATION_DB_NAME \
                  --dbuser=$CONFIGURATION_DB_USER \
                  --dbpass=$CONFIGURATION_DB_PASSWORD \
-                 --dbhost=$CONFIGURATION_DB_HOST --allow-root 
+                 --dbhost=$CONFIGURATION_DB_HOST --allow-root
 
 wp core install --url=localhost \
                 --title=inception42 \
@@ -20,5 +17,10 @@ wp core install --url=localhost \
                 --admin_email=$WP_ADMIN_EMAIL --allow-root
 
 wp user create $author_name $author_email --role=contributer --user_pass=$author_pass --allow-root
+
+
+# Install and activate the Redis Object Cache plugin BONUS
+sudo -u www-data wp plugin install redis-cache --activate
+sudo -u www-data wp redis enable
 
 /usr/sbin/php-fpm7.4 -F
